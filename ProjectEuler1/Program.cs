6902e0aa05cac0e0d9c575a3c169c5cf717f5b8d
pgellert <gellpn22@gmail.com>
2015-12-17 23:18:10 +0100

    Project euler problem 1

---
+++ ProjectEuler1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ProjectEuler1
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			int n = 1000;
+			int sum = 0;
+
+			for (int i = 1; i < n; ++i) {
+				sum += (i % 3 == 0 || i % 5 == 0) ? i : 0;
+			}
+			Console.WriteLine (sum);
+		}
+	}
+}
